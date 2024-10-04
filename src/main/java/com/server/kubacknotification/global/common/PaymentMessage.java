1c844bd74b8222af154764652fa15bb6a6361b4c
codrin2 <hssang127@gmail.com>
2024-10-04 16:40:52 +0900

    [feat] email, kafka config 설정

---
+++ src/main/java/com/server/kubacknotification/global/common/PaymentMessage.java
@@ -0,0 +1,24 @@
+package com.server.kubacknotification.global.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentMessage {
+	private Long userId;
+	private String seatNumber;
+	private String seatGrade;
+	private String payment;
+	private LocalDateTime payDate;
+	private String originalPrice;
+	private String salePrice;
+	private String finalPrice;
+}
+
